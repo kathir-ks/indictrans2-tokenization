5e4296f669f6efb31e0ceae73d7f53330b959bd9
kathir-ks <kathirksw@gmail.com>
2024-04-09 19:53:32 +0530

    Add args parser

--- tokenize_sentences.py
+++ tokenize_sentences.py
@@ -8,6 +8,39 @@ import nltk
 nltk.download('punkt')
 
 
+data_files = {
+   "auto_math_text":{"data/auto_math_text/train-000**-of-00018.parquet"},
+   "combined":{"data/openstax/train-0000*-of-00002.parquet",
+               "data/khanacademy/train-00000-of-00001.parquet", 
+               "data/wikihow/train-0000*-of-00002.parquet"
+               },
+  "stanford":{"data/stanford/train-000**-of-00013.parquet"},
+  "stories_shard_1":{f"data/stories/train-{str(i).zfill(5)}-of-00043.parquet" for i in range(0, 14)},
+  "stories_shard_2":{f"data/stories/train-{str(i).zfill(5)}-of-00043.parquet" for i in range(14, 28)},
+  "stories_shard_3":{f"data/stories/train-{str(i).zfill(5)}-of-00043.parquet" for i in range(28, 43)},
+  "web_samples_v1_shard_1":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(0, 14)},
+  "web_samples_v1_shard_2":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(14, 28)},
+  "web_samples_v1_shard_3":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(28, 43)},
+  "web_samples_v1_shard_4":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(43, 57)},
+  "web_samples_v1_shard_5":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(57, 71)},
+  "web_samples_v1_shard_6":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(71, 86)},
+  "web_samples_v1_shard_7":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(86, 100)},
+  "web_samples_v1_shard_8":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(100, 114)},
+  "web_samples_v1_shard_9":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(114, 129)},
+  "web_samples_v1_shard_10":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(129, 139)},
+  "web_samples_v2_shard_1":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(0, 12)},
+  "web_samples_v2_shard_2":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(12, 24)},
+  "web_samples_v2_shard_3":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(24, 36)},
+  "web_samples_v2_shard_4":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(36, 48)},
+  "web_samples_v2_shard_5":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(48, 60)},
+  "web_samples_v2_shard_6":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(60, 72)},
+  "web_samples_v2_shard_7":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(72, 84)},
+  "web_samples_v2_shard_8":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(84, 96)},
+  "web_samples_v2_shard_9":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(96, 108)},
+  "web_samples_v2_shard_10":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(108, 118)}
+}
+
+
 def write_json(data, filename):
     with open(filename, 'w') as f:
         json.dump(data, f)
@@ -60,68 +93,6 @@ def tokenize_sentences(sentences,indices, tokenizer, ip, src_lang, tgt_lang):
 
     return {"indices": indices, "tokenized_input": inputs}
 
-data_files = {
-   "auto_math_text":{"data/auto_math_text/train-000**-of-00018.parquet"},
-   "combined":{"data/openstax/train-0000*-of-00002.parquet",
-               "data/khanacademy/train-00000-of-00001.parquet", 
-               "data/wikihow/train-0000*-of-00002.parquet"
-               },
-  "stanford":{"data/stanford/train-000**-of-00013.parquet"},
-  "stories_shard_1":{f"data/stories/train-{str(i).zfill(5)}-of-00043.parquet" for i in range(0, 14)},
-  "stories_shard_2":{f"data/stories/train-{str(i).zfill(5)}-of-00043.parquet" for i in range(14, 28)},
-  "stories_shard_3":{f"data/stories/train-{str(i).zfill(5)}-of-00043.parquet" for i in range(28, 43)},
-  "web_samples_v1_shard_1":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(0, 14)},
-  "web_samples_v1_shard_2":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(14, 28)},
-  "web_samples_v1_shard_3":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(28, 43)},
-  "web_samples_v1_shard_4":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(43, 57)},
-  "web_samples_v1_shard_5":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(57, 71)},
-  "web_samples_v1_shard_6":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(71, 86)},
-  "web_samples_v1_shard_7":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(86, 100)},
-  "web_samples_v1_shard_8":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(100, 114)},
-  "web_samples_v1_shard_9":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(114, 129)},
-  "web_samples_v1_shard_10":{f"data/web_samples_v1/train-{str(i).zfill(5)}-of-00139.parquet" for i in range(129, 139)},
-  "web_samples_v2_shard_1":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(0, 12)},
-  "web_samples_v2_shard_2":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(12, 24)},
-  "web_samples_v2_shard_3":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(24, 36)},
-  "web_samples_v2_shard_4":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(36, 48)},
-  "web_samples_v2_shard_5":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(48, 60)},
-  "web_samples_v2_shard_6":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(60, 72)},
-  "web_samples_v2_shard_7":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(72, 84)},
-  "web_samples_v2_shard_8":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(84, 96)},
-  "web_samples_v2_shard_9":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(96, 108)},
-  "web_samples_v2_shard_10":{f"data/web_samples_v2/train-{str(i).zfill(5)}-of-000118.parquet" for i in range(108, 118)}
-}
-
-direction = "en-indic"
-
-ip = IndicProcessor(inference=True)
-tokenizer = IndicTransTokenizer(direction=direction)
-
-dataset = load_dataset("HuggingFaceTB/cosmopedia", data_files=data_files[subset])
-dataset = dataset['train']
-dataset = dataset['text']
-
-results = []
-
-with ThreadPoolExecutor(max_workers=96) as executor:
-       results.extend(executor.map(split_into_sentences, range(len(dataset)), dataset))
-
-indices = []
-sentences = []
-
-for result in results:   
-    assert len(result[0])==len(result[1])
-    indices.extend(result[0])
-    sentences.extend(result[1])
-
-assert len(indices)==len(sentences)
-
-data = []
-
-with ThreadPoolExecutor(max_workers=96) as executor:
-         data.extend(executor.map(tokenize_sentences, (sentences[i : i + tonkenization_batch_size] for i in range(0, len(sentences), tonkenization_batch_size)),
-                                  (indices[i : i + tonkenization_batch_size] for i in range(0, len(indices), tonkenization_batch_size)),
-                                   tokenizer, ip, src_lang, tgt_lang))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tokenize sentences')
@@ -129,4 +100,47 @@ if __name__ == '__main__':
     parser.add_argument("--src_lang", default="eng_Latn", type=str, required=False)
     parser.add_argument("--tgt_lang", default=None, type=str, required=True)
     parser.add_argument("--direction", default="en-indic", type=str, required=False)
-    parser.add_argument("--tokenization_batch_size", default=64, type=int, required=False)
+    parser.add_argument("--tokenization_batch_size", default=64, type=int, required=True)
+
+    args = parser.parse_args()
+
+    subset = args.subset
+    src_lang = args.src_lang
+    tgt_lang = args.tgt_lang
+    direction = args.direction
+    tonkenization_batch_size = args.tokenization_batch_size
+
+    assert subset in data_files.keys()
+    assert tgt_lang is not None
+
+    ip = IndicProcessor(inference=True)
+    tokenizer = IndicTransTokenizer(direction=direction)
+
+    dataset = load_dataset("HuggingFaceTB/cosmopedia", data_files=data_files[subset])
+    dataset = dataset['train']
+    dataset = dataset['text']
+
+    results = []
+
+    with ThreadPoolExecutor(max_workers=96) as executor:
+           results.extend(executor.map(split_into_sentences, range(len(dataset)), dataset))
+
+    indices = []
+    sentences = []
+
+    for result in results:   
+        assert len(result[0])==len(result[1])
+        indices.extend(result[0])
+        sentences.extend(result[1])
+
+    assert len(indices)==len(sentences)
+
+    data = []
+
+    with ThreadPoolExecutor(max_workers=96) as executor:
+            data.extend(executor.map(tokenize_sentences, (sentences[i : i + tonkenization_batch_size] for i in range(0, len(sentences), tonkenization_batch_size)),
+                                    (indices[i : i + tonkenization_batch_size] for i in range(0, len(indices), tonkenization_batch_size)),
+                                    tokenizer, ip, src_lang, tgt_lang))    
+
+
+
