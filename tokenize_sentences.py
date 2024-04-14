555d0974e4dae9b9099e42299fa72315053de25e
kathir <kathirksw@gmail.com>
2024-04-14 17:06:36 +0530

    ProcessPoolExecutor instead of ThreadPoolExecutor

--- tokenize_sentences.py
+++ tokenize_sentences.py
@@ -3,19 +3,21 @@ from datasets import load_dataset
 import argparse
 from IndicTransTokenizer import IndicProcessor, IndicTransTokenizer
 import time
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 from itertools import repeat
 import nltk
 nltk.download('punkt')
 
 
 data_files = {
-   "auto_math_text":{"data/auto_math_text/train-0000*-of-00018.parquet"},
-   "combined":{"data/openstax/train-0000*-of-00002.parquet",
+   "auto_math_text":{f"data/auto_math_text/train-{str(i).zfill(5)}-of-00018.parquet" for i in range(0, 18)},
+   "combined":{"data/openstax/train-00000-of-00002.parquet",
+               "data/openstax/train-00001-of-00002.parquet",
                "data/khanacademy/train-00000-of-00001.parquet", 
-               "data/wikihow/train-0000*-of-00002.parquet"
+               "data/wikihow/train-00000-of-00002.parquet",
+               "data/wikihow/train-00000-of-00002.parquet"
                },
-  "stanford":{"data/stanford/train-0000*-of-00013.parquet"},
+  "stanford":{f"data/stanford/train-{str(i).zfill(5)}-of-00013.parquet" for i in range(0, 13)},
   "stories_shard_1":{f"data/stories/train-{str(i).zfill(5)}-of-00043.parquet" for i in range(0, 14)},
   "stories_shard_2":{f"data/stories/train-{str(i).zfill(5)}-of-00043.parquet" for i in range(14, 28)},
   "stories_shard_3":{f"data/stories/train-{str(i).zfill(5)}-of-00043.parquet" for i in range(28, 43)},
@@ -96,12 +98,15 @@ def tokenize_sentences(sentences,indices, tokenizer, ip, src_lang, tgt_lang):
 
 
 if __name__ == '__main__':
+    
     parser = argparse.ArgumentParser(description='Tokenize sentences')
     parser.add_argument("--subset", default=None, type=str, required=True, help=f"{data_files.keys()}")
     parser.add_argument("--src_lang", default="eng_Latn", type=str, required=False)
     parser.add_argument("--tgt_lang", default=None, type=str, required=True)
     parser.add_argument("--direction", default="en-indic", type=str, required=False)
     parser.add_argument("--tokenization_batch_size", default=64, type=int, required=True)
+    parser.add_argument("--max_workers", default=96, type=int, required=True)
+
 
     args = parser.parse_args()
 
@@ -110,6 +115,7 @@ if __name__ == '__main__':
     tgt_lang = args.tgt_lang
     direction = args.direction
     tonkenization_batch_size = args.tokenization_batch_size
+    max_workers = args.max_workers
 
     assert subset in data_files.keys()
     assert tgt_lang is not None
@@ -123,7 +129,7 @@ if __name__ == '__main__':
 
     results = []
 
-    with ThreadPoolExecutor(max_workers=96) as executor:
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
            results.extend(executor.map(split_into_sentences, range(len(dataset)), dataset))
 
     indices = []
@@ -138,7 +144,7 @@ if __name__ == '__main__':
 
     data = []
 
-    with ThreadPoolExecutor(max_workers=96) as executor:
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
             data.extend(executor.map(tokenize_sentences, (sentences[i : i + tonkenization_batch_size] for i in range(0, len(sentences), tonkenization_batch_size)),
                                     (indices[i : i + tonkenization_batch_size] for i in range(0, len(indices), tonkenization_batch_size)),
                                     repeat(tokenizer), repeat(ip), repeat(src_lang), repeat(tgt_lang)))    
@@ -146,5 +152,4 @@ if __name__ == '__main__':
     file_name = f"{subset}.json"
     write_json(data, file_name)
     
-
 
